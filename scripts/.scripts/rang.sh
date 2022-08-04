2906d0835c2d04f98d24efb82f0a3eea6ebf52d6
Mohamad Supangat <hantamkoding@gmail.com>
2022-08-05 02:32:40 +0700

    add some scripts

---
+++ scripts/.scripts/rang.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# variables
+bordercolor="#303030"
+border=5
+image="/tmp/image.png"
+icon="/tmp/icon.png"
+
+grim -g "$(slurp -p)" - > $image
+hexcode=$(magick /tmp/pixel.png -colors 1 txt: | awk 'NR==2 {print $3}')
+magick convert $image -filter point -resize 96x96 -bordercolor $bordercolor -border $border $icon
+notify-send -i $icon "  rang" "The hex code is $hexcode"
+wl-copy -n $hexcode
+rm -f $image $icon

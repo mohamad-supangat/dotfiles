eddbc2984e9c56849d3d29abc4f190a34f29b089
Mohamad Supangat <hantamkoding@gmail.com>
2022-08-09 15:51:08 +0700

    add some argument in tmux_session rofi .sh

--- scripts/.scripts/tmux_session.sh
+++ scripts/.scripts/tmux_session.sh
@@ -6,13 +6,17 @@ function tmux_sessions()
 {
     tmux list-session -F '#S'
 }
-
-TMUX_SESSION=$( (echo new; tmux_sessions) | rofi -dmenu -p "Select tmux session")
-
-if [[ x"new" = x"${TMUX_SESSION}" ]]; then
-    rofi-sensible-terminal -e tmux new-session &
+TMUX_SESSIONS=$(tmux_sessions)
+TMUX_SESSION=$( (tmux_sessions) | rofi -dmenu -p "Select tmux session")
+TMUX_SESSIONS=(${TMUX_SESSIONS//$'\n'/ })
+echo $TMUX_SESSIONS
+echo $TMUX_SESSION
+if [[ " ${TMUX_SESSIONS[*]} " =~ " ${TMUX_SESSION} " ]]; then
+    echo "select exiting"
+    rofi-sensible-terminal -e tmux attach -d -t "${TMUX_SESSION}" &
 elif [[ -z "${TMUX_SESSION}" ]]; then
     echo "Cancel"
 else
-    rofi-sensible-terminal -e tmux attach -d -t "${TMUX_SESSION}" &
+    echo "new session"
+    rofi-sensible-terminal -e tmux new-session -t "${TMUX_SESSION}" &
 fi

e1d13eea3c7ebb7c47e745b32f86a31cdd570d3a
Mohamad Supangat <hantamkoding@gmail.com>
2022-09-10 14:00:22 +0700

    fix starting vnc in sway headles

--- sway/.config/sway/bin/start-vnc.sh
+++ sway/.config/sway/bin/start-vnc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec env WLR_BACKENDS=headless WLR_LIBINPUT_NO_DEVICES=1 sway --config ~/.config/sway/remote-config
+exec env WLR_BACKENDS=headless WLR_LIBINPUT_NO_DEVICES=1 WAYLAND_DISPLAY=wayland-1 sway --config ~/.config/sway/remote-config

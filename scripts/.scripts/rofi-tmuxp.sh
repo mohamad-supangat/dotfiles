989aaae94301be89aaf0bd8ff49c8ec0f0352d27
Mohamad Supangat <moha.supangat@gmail.com>
2024-05-20 22:00:44 +0700

    scripts: new script rofi-tmuxp

--- scripts/.scripts/rofi-tmuxp.sh
+++ scripts/.scripts/rofi-tmuxp.sh
@@ -1,1 +1,6 @@
-rofi -modi tmuxp:rofi-tmuxp -show tmuxp
+#!/bin/env bash
+dir="$HOME/.tmuxp/"
+selected_project=$(ls $dir | fuzzel --dmenu $projects)
+# echo $TERMINAL
+echo $dir$selected_project
+$TERMINAL -e tmuxp load $dir$selected_project

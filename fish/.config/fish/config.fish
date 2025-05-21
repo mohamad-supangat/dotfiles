c0b0a534719f3339cff66090568c2f34925bce48
Mohamad Supangat <moha.supangat@gmail.com>
2025-05-21 08:11:27 +0700

    php-env

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -31,27 +31,31 @@ alias tmuxproject="tmuxinator"
 alias se="sudo -E"
 alias composer7="/usr/bin/php7 /usr/bin/composer"
 alias update-font-cache="fc-cache -f -v"
-alias vi="vim"
-alias v="vim"
+
+# neovim drop in
+alias vi="nvim"
+alias v="nvim"
+alias vim="nvim"
+
 alias create-laravel-project="composer create-project laravel/laravel"
 alias create-vite-project="pnpm create vite"
 alias readme-md-generator="pnpx readme-md-generator"
-alias odoo-readme-md-to-description="mkdir -p static/description && github-markdown README.md > static/description/index.html"
-alias adb-push-public-key="adb push ~/.android/adbkey.pub /data/misc/adb/adb_keys"
+# alias odoo-readme-md-to-description="mkdir -p static/description && github-markdown README.md > static/description/index.html"
+# alias adb-push-public-key="adb push ~/.android/adbkey.pub /data/misc/adb/adb_keys"
 alias adb-screenshot="adb exec-out screencap -p > ~/Pictures/screenshots/android-$(date +"%Y-%m-%d-%H-%M-%S").png"
 alias tmux-save-pane='tmux capture-pane -pS -'
 alias pnpm-install-aura-erp-installer="pnpm add -g git+ssh://git@gitlab.com:aura-erp/installer.git"
 alias pac="sudo pacman"
 alias comigo="comi -c ~/.config/comigo/config.toml"
 
+# alias sync-odoo-addons-pangat="rsync -azP ~/odoo/addons/ pangat@192.168.10.112:/home/pangat/odoo-15/addons/"
+# alias sync-odoo-addons="rsync -azP ~/odoo/addons/ root@192.168.10.112:/home/saverid/odoo_22/addons/"
 
-alias sync-odoo-addons-pangat="rsync -azP ~/odoo/addons/ pangat@192.168.10.112:/home/pangat/odoo-15/addons/"
-alias sync-odoo-addons="rsync -azP ~/odoo/addons/ root@192.168.10.112:/home/saverid/odoo_22/addons/"
+# alias pulseaudi-stream-simple-protocol-tcp="pactl load-module module-simple-protocol-tcp source=102 record=true port=12345 && adb reverse tcp:12345 tcp:12345"
+# alias nchat-wa="nchat -d ~/.nchat-main-wa"
+# alias release-it-gitlab="release-it --gitlab.release"
+# alias release-it-github="release-it --github.release"
 
-alias pulseaudi-stream-simple-protocol-tcp="pactl load-module module-simple-protocol-tcp source=102 record=true port=12345 && adb reverse tcp:12345 tcp:12345"
-alias nchat-wa="nchat -d ~/.nchat-main-wa"
-alias release-it-gitlab="release-it --gitlab.release"
-alias release-it-github="release-it --github.release"
 # alias mangal="docker run --rm -ti -e \"TERM=xterm-256color\" -v ~/Downloads/Manga/:/downloads -v ~/.config/mangal/config:/config metafates/mangal"
 # alias glab-jvm="GITLAB_TOKEN=$GITLAB_JVM_TOKEN glab"
 # alias release-it-jvm="GITLAB_TOKEN=$GITLAB_JVM_TOKEN release-it --gitlab.release"
@@ -84,11 +88,11 @@ fish_add_path /opt/flutter/bin/
 # fish_add_path $ANDROID_HOME/platform-tools
 # fish_add_path $ANDROID_HOME/cmdline-tools/bin
 # fish_add_path $ANDROID_HOME/tools
-#
-#
-# set -x PHPENV_ROOT "/home/deve/.phpenv"
+
+
+# phpenv
+set -x PHPENV_ROOT "/home/deve/.phpenv"
 if test -d "/home/deve/.phpenv"
   set -x PATH "/home/deve/.phpenv/bin" $PATH
   status --is-interactive; and . (phpenv init -|psub)
 end
-

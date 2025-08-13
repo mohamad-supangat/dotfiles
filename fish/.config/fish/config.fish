f0fda2970bbbfbd8417ab78aa0eb813478cc480b
Mohamad Supangat <moha.supangat@gmail.com>
2025-08-13 08:17:28 +0700

    waybar: fix focused workspace color

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,12 @@ fish_add_path /opt/android-sdk/build-tools/30.0.3
 fish_add_path ~/go/bin
 
 
+if test -f "~/.private-env"
+  source ~/.private-env
+end
+
+set --export EDITOR nvim
+
 # init starship default prompts
 # starship init fish | source
 

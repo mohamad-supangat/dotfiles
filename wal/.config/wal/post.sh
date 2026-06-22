3e9b01fd99921192225ae0d2bdac451f3bf7cfe0
Mohamad Supangat <moha.supangat@gmail.com>
2026-06-22 08:42:46 +0700

    pywal16 reload neovim theme

--- wal/.config/wal/post.sh
+++ wal/.config/wal/post.sh
@@ -7,13 +7,14 @@ else
 fi
 
 if command -v vibepanel &>/dev/null; then
-    # Jika sudah berjalan, kirim sinyal USR1. Jika belum, abaikan error (|| true)
     pgrep -x "vibepanel" &>/dev/null && pkill -USR1 vibepanel || true
-
-    # Jalankan vibepanel di background dan pisahkan dari terminal (disown)
     vibepanel &
     disown
 else
     echo "Error: 'vibepanel' tidak ditemukan. Script dihentikan." >&2
     exit 1
 fi
+
+
+
+pkill -SIGUSR1 nvim

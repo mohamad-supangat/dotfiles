a950c0e474048a42efe6fac3cc612f3045a2397a
Mohamad Supangat <moha.supangat@gmail.com>
2026-08-26 15:48:16 +0700

    update some theme config

--- niri/.config/matugen/post-hook-scripts/gtk-theme-reload.sh
+++ niri/.config/matugen/post-hook-scripts/gtk-theme-reload.sh
@@ -3,9 +3,9 @@
 current=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ "$current" == "'prefer-dark'" ]]; then
-    gsettings set org.gnome.desktop.interface color-scheme prefer-light
-    gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+  gsettings set org.gnome.desktop.interface color-scheme prefer-light
+  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 else
-    gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-    gsettings set org.gnome.desktop.interface color-scheme prefer-light
+  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+  gsettings set org.gnome.desktop.interface color-scheme prefer-light
 fi

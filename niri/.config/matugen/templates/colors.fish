ecd4760f5d0878230fa0d6080dfe51792903d129
Mohamad Supangat <moha.supangat@gmail.com>
2026-08-27 13:39:19 +0700

    update gtk and color fish

--- niri/.config/matugen/templates/colors.fish
+++ niri/.config/matugen/templates/colors.fish
@@ -1,40 +1,39 @@
-set fish_color_autosuggestion {{colors.outline.default.hex_stripped}}
-set fish_color_cancel -r
-set fish_color_command {{colors.primary.default.hex_stripped}}
-set fish_color_comment {{colors.outline.default.hex_stripped}}
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_end {{colors.tertiary_fixed_dim.default.hex_stripped}}
+set fish_color_autosuggestion {{colors.surface_variant.default.hex_stripped}}
+set fish_color_cancel {{colors.error.default.hex_stripped}} --reverse
+set fish_color_command {{colors.tertiary_container.default.hex_stripped}}
+set fish_color_comment {{colors.surface_variant.default.hex_stripped}}
+set fish_color_cwd {{colors.tertiary.default.hex_stripped}}
+set fish_color_cwd_root {{colors.error.default.hex_stripped}}
+set fish_color_end {{colors.secondary.default.hex_stripped}}
 set fish_color_error {{colors.error.default.hex_stripped}}
-set fish_color_escape {{colors.outline.default.hex_stripped}}
+set fish_color_escape {{colors.primary_fixed.default.hex_stripped}}
 set fish_color_history_current --bold
-set fish_color_host normal
-set fish_color_host_remote yellow
-set fish_color_keyword {{colors.primary.default.hex_stripped}}
-set fish_color_match {{colors.tertiary.default.hex_stripped}}
-set fish_color_normal normal
-set fish_color_operator {{colors.tertiary.default.hex_stripped}}
-set fish_color_option {{colors.inverse_primary.default.hex_stripped}}
+set fish_color_host {{colors.inverse_primary.default.hex_stripped}}
+set fish_color_host_remote {{colors.inverse_primary.default.hex_stripped}}
+set fish_color_keyword {{colors.primary_fixed.default.hex_stripped}}
+set fish_color_match --background={{colors.primary.default.hex_stripped}}
+set fish_color_normal {{colors.on_background.default.hex_stripped}}
+set fish_color_operator {{colors.secondary_fixed.default.hex_stripped}}
+set fish_color_option {{colors.secondary.default.hex_stripped}}
 set fish_color_param {{colors.inverse_primary.default.hex_stripped}}
-set fish_color_quote {{colors.secondary.default.hex_stripped}}
-set fish_color_redirection {{colors.inverse_primary.default.hex_stripped}}
-
-set fish_color_search_match bryellow '--background=brblack'
-set fish_color_selection white --bold '--background=brblack'
-set fish_color_status red
-set fish_color_user brgreen
+set fish_color_quote {{colors.secondary_container.default.hex_stripped}}
+set fish_color_redirection {{colors.primary_fixed.default.hex_stripped}}
+set fish_color_search_match --background={{colors.surface_variant.default.hex_stripped}}
+set fish_color_selection --background={{colors.surface_variant.default.hex_stripped}}
+set fish_color_status {{colors.error.default.hex_stripped}}
+set fish_color_user {{colors.tertiary_container.default.hex_stripped}}
 set fish_color_valid_path --underline
 
-set fish_pager_color_background
-set fish_pager_color_completion normal
-set fish_pager_color_description B3A06D yellow
-set fish_pager_color_prefix normal --bold --underline
-set fish_pager_color_progress brwhite '--background=cyan'
-set fish_pager_color_secondary_background
-set fish_pager_color_secondary_completion
-set fish_pager_color_secondary_description
-set fish_pager_color_secondary_prefix
-set fish_pager_color_selected_background --background=brblack
-set fish_pager_color_selected_completion
-set fish_pager_color_selected_description
-set fish_pager_color_selected_prefix
+set fish_pager_color_background {{colors.background.default.hex_stripped}}
+set fish_pager_color_completion {{colors.on_background.default.hex_stripped}}
+set fish_pager_color_description {{colors.surface_variant.default.hex_stripped}}
+set fish_pager_color_prefix {{colors.tertiary_container.default.hex_stripped}}
+set fish_pager_color_progress {{colors.surface_variant.default.hex_stripped}}
+set fish_pager_color_secondary_background {{colors.background.default.hex_stripped}}
+set fish_pager_color_secondary_completion {{colors.on_background.default.hex_stripped}}
+set fish_pager_color_secondary_description {{colors.surface_variant.default.hex_stripped}}
+set fish_pager_color_secondary_prefix {{colors.tertiary_container.default.hex_stripped}}
+set fish_pager_color_selected_background --background={{colors.surface_variant.default.hex_stripped}}
+set fish_pager_color_selected_completion {{colors.on_background.default.hex_stripped}}
+set fish_pager_color_selected_description {{colors.surface_variant.default.hex_stripped}}
+set fish_pager_color_selected_prefix {{colors.tertiary_container.default.hex_stripped}}

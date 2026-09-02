ca9db9780451881caaf66cc6f4447af01b08a878
Mohamad Supangat <moha.supangat@gmail.com>
2026-09-02 21:15:14 +0700

    matugen: add nvim lua template

---
+++ niri/.config/matugen/templates/nvim.lua
@@ -0,0 +1,39 @@
+return {
+  wallpaper = "{{image}}",
+
+  background = "{{colors.surface.default.hex}}",
+  foreground = "{{colors.on_surface.default.hex}}",
+  cursor = "{{colors.on_surface.default.hex}}",
+
+  -- Black
+  color0 = "{{colors.surface.default.hex}}",
+  color8 = "{{colors.outline.default.hex}}",
+
+  -- Red
+  color1 = "{{colors.error.default.hex}}",
+  color9 = "{{colors.error_container.default.hex}}",
+
+  -- Green
+  color2 = "{{colors.primary.default.hex}}",
+  color10 = "{{colors.primary.default.hex}}",
+
+  -- Yellow
+  color3 = "{{colors.secondary.default.hex}}",
+  color11 = "{{colors.secondary.default.hex}}",
+
+  -- Blue
+  color4 = "{{colors.tertiary.default.hex}}",
+  color12 = "{{colors.tertiary.default.hex}}",
+
+  -- Magenta
+  color5 = "{{colors.primary_container.default.hex}}",
+  color13 = "{{colors.primary_container.default.hex}}",
+
+  -- Cyan
+  color6 = "{{colors.secondary_container.default.hex}}",
+  color14 = "{{colors.secondary_container.default.hex}}",
+
+  -- White
+  color7 = "{{colors.on_surface.default.hex}}",
+  color15 = "{{colors.on_surface_variant.default.hex}}"
+}

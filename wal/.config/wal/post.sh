5f8b161be3c6b551f7e1521b385dafad04fb8b87
Mohamad Supangat <moha.supangat@gmail.com>
2026-09-03 08:23:05 +0700

    feat: penambahan vibepanel

--- wal/.config/wal/post.sh
+++ wal/.config/wal/post.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
 if command -v scrollmsg &>/dev/null; then
-    scrollmsg reload
+  scrollmsg reload
 else
-    echo "Peringatan: 'scrollmsg' tidak ditemukan, melewati proses reload." >&2
+  echo "Peringatan: 'scrollmsg' tidak ditemukan, melewati proses reload." >&2
 fi
 
 if command -v vibepanel &>/dev/null; then
-    pgrep -x "vibepanel" &>/dev/null && pkill -USR1 vibepanel || true
-    vibepanel &
-    disown
+  pgrep -x "vibepanel" &>/dev/null && pkill -USR1 vibepanel || true
+  vibepanel &
+  disown
 else
-    echo "Error: 'vibepanel' tidak ditemukan. Script dihentikan." >&2
-    exit 1
+  echo "Error: 'vibepanel' tidak ditemukan. Script dihentikan." >&2
+  exit 1
 fi
-
-
 
 pkill -SIGUSR1 nvim
